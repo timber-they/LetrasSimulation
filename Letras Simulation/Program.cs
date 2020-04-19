0760639f6cc6cc5b10a06e15a362a2c423ed0e28
MetaColon <timo@teemze.de>
2020-04-19 16:24:50 +0200

    Multiple tours & better formatting

--- Letras Simulation/Program.cs
+++ Letras Simulation/Program.cs
@@ -10,14 +10,24 @@ namespace Letras_Simulation
 {
     internal class Program
     {
-        private const string DATA = "Resources/Data.gpx";
+        private static string _data;
+        private const  string RESOURCES = "Resources/";
+        private const  string ENDING    = ".gpx";
 
         public static void Main (string [] args)
         {
             start:
 
-            Console.WriteLine ("Do you want continuous power or time calculation?");
+            Console.WriteLine ("|\n| Which tour do you want to analyze?\n|");
             var input = Console.ReadLine ();
+            if (input == null)
+                goto start;
+            _data = RESOURCES + input + ENDING;
+            if (!File.Exists (_data))
+                goto start;
+
+            Console.WriteLine ("|\n| Do you want continuous power or time calculation?\n|");
+            input = Console.ReadLine ();
             if (input == null)
                 goto start;
             if (input.ToLower ().StartsWith ("p"))
@@ -36,7 +46,7 @@ namespace Letras_Simulation
             start:
             double power;
             do
-                Console.WriteLine ("Please enter your power");
+                Console.WriteLine ("|\n| Please enter your power\n|");
             while (!double.TryParse (Console.ReadLine (), NumberStyles.Any, CultureInfo.InvariantCulture, out power));
 
             TimeCalculation (power);
@@ -45,21 +55,21 @@ namespace Letras_Simulation
 
         private static int TimeCalculation (double power)
         {
-            Console.WriteLine ("Calculating time...");
+            Console.WriteLine ("|\n| Calculating time...\n|");
 
             var elapsedTime = CalculateTime (power);
 
             var hours   = (int) (elapsedTime / 3600.0);
             var minutes = (int) (elapsedTime / 60.0 - hours * 60.0);
             var seconds = (int) (elapsedTime - hours * 3600.0 - minutes * 60.0);
-            Console.WriteLine ($"This will take {elapsedTime} s, meaning about {hours} h, {minutes} m, {seconds} s");
+            Console.WriteLine ($"|\n| This will take {elapsedTime} s, meaning about {hours} h, {minutes} m, {seconds} s\n|");
 
             return (int) elapsedTime;
         }
 
         private static double CalculateTime (double power, double precision = 0.01, Tour tour = null)
         {
-            tour = tour ?? XmlUtil.Parse (File.ReadAllText (DATA));
+            tour = tour ?? XmlUtil.Parse (File.ReadAllText (_data));
             // Me, my bike, my bottles and other stuff
             var state = new State (tour, 66.2 + 7.9 + 1.5 + 2.0);
 
@@ -81,11 +91,11 @@ namespace Letras_Simulation
             start:
             int hours;
             do
-                Console.WriteLine ("Please enter the hours");
+                Console.WriteLine ("|\n| Please enter the hours\n|");
             while (!int.TryParse (Console.ReadLine (), NumberStyles.Any, CultureInfo.InvariantCulture, out hours));
             int minutes;
             do
-                Console.WriteLine ("Please enter the minutes");
+                Console.WriteLine ("|\n| Please enter the minutes\n|");
             while (!int.TryParse (Console.ReadLine (), NumberStyles.Any, CultureInfo.InvariantCulture, out minutes));
 
             var power = PowerCalculation (hours * 3600 + minutes * 60);
@@ -95,15 +105,15 @@ namespace Letras_Simulation
 
         private static int PowerCalculation (int seconds)
         {
-            Console.WriteLine ("Calculating power...");
+            Console.WriteLine ("|\n| Calculating power...\n|");
             var power = CalculatePower (seconds);
-            Console.WriteLine ($"This will need {power} W");
+            Console.WriteLine ($"|\n| This will need {power} W\n|");
             return power;
         }
 
         private static int CalculatePower (int seconds)
         {
-            var tour = XmlUtil.Parse (File.ReadAllText (DATA));
+            var tour = XmlUtil.Parse (File.ReadAllText (_data));
 
             var stepLength = 500.0;
             var current    = stepLength;
